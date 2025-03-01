80ea03b05121418e62d8f9e69261efb0510fffb7
Artur2 <arturnurullin@outlook.com>
2025-03-01 18:44:59 +0500

    formatting

--- src/in_memory_todo_manager.rs
+++ src/in_memory_todo_manager.rs
@@ -70,7 +70,7 @@ impl TodoManager for InMemoryTodoManager {
         });
     }
 
-   async fn is_task_exist(&mut self, title: &str) -> bool {
+    async fn is_task_exist(&mut self, title: &str) -> bool {
         match self.get(title) {
             Some(_) => true,
             None => false,
